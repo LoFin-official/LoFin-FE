d4c6f6a15a74ae7b48b78d60693cee48297826c6
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-04-21 00:53:34 +0900

    PR 템플릿 생성

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,24 @@
 import React, { ReactNode } from 'react';
 
 interface ButtonProps {
-    children: ReactNode;
-    className?: string;
-    isComplete: boolean;
-    onClick?: () => void;
+  children: ReactNode;
+  className?: string;
+  isComplete: boolean;
+  onClick?: () => void;
 }
 
 export default function Button({ children, className, isComplete, onClick }: ButtonProps) {
-    return (
-        <div className={className} onClick={onClick}>
-            {isComplete ? (
-                <div className='w-[380px] h-[50px] flex items-center justify-center rounded-md bg-[#FF9BB3] mx-auto cursor-pointer hover:bg-[#FF4C80]'>
-                    <span className='text-xl font-bold text-white'>{children}</span>
-                </div>
-            ) : (
-                <div className='w-[380px] h-[50px] flex items-center justify-center rounded-md bg-[#999999] mx-auto cursor-pointer hover:bg-[#8A8A8A]'>
-                    <span className='text-xl font-bold text-white'>{children}</span>
-                </div>
-            )}
+  return (
+    <div className={className} onClick={onClick}>
+      {isComplete ? (
+        <div className='w-[380px] h-[50px] flex items-center justify-center rounded-md bg-[#FF9BB3] mx-auto cursor-pointer hover:bg-[#FF4C80]'>
+          <span className='text-xl font-bold text-white'>{children}</span>
         </div>
-    )
+      ) : (
+        <div className='w-[380px] h-[50px] flex items-center justify-center rounded-md bg-[#999999] mx-auto cursor-pointer hover:bg-[#8A8A8A]'>
+          <span className='text-xl font-bold text-white'>{children}</span>
+        </div>
+      )}
+    </div>
+  );
 }
