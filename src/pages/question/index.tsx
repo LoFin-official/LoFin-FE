bfa99cef6a8c0cc11131bdace2b59aec5d218d07
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-04-21 18:23:03 +0900

    📝 Update : 헤더 적용

--- src/pages/question/index.tsx
+++ src/pages/question/index.tsx
@@ -1,14 +1,16 @@
 import BottomBar from '@/components/shared/BottomBar';
+import Header from '@/components/shared/Header';
 import React from 'react';
 import { ReactNode } from 'react';
 
 export default function QuestionPage() {
-  return <div>질문페이지입니당.</div>;
+  return;
 }
 
 QuestionPage.getLayout = (page: ReactNode) => {
   return (
     <>
+      <Header>질문</Header>
       <BottomBar>{page}</BottomBar>
     </>
   );
