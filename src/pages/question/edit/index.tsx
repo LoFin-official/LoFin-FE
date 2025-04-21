46c095f01a4936d2e433def79ff8d17ac6e49136
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-04-21 18:23:22 +0900

    🌟 Feat : 질문 생성 페이지 추가

---
+++ src/pages/question/edit/index.tsx
@@ -0,0 +1,25 @@
+import QuestionTabs from '@/components/QuestionTabs';
+import BottomBar from '@/components/shared/BottomBar';
+import Header from '@/components/shared/Header';
+import { ReactNode } from 'react';
+import React, { useState } from 'react';
+
+export default function QuestionEditPage() {
+  const [selectedTab, setSelectedTab] = useState<'direct' | 'random'>('direct');
+
+  return (
+    <div>
+      {/* 탭 컴포넌트 */}
+      <QuestionTabs selectedTab={selectedTab} onSelectTab={setSelectedTab} />
+    </div>
+  );
+}
+
+QuestionEditPage.getLayout = (page: ReactNode) => {
+  return (
+    <>
+      <Header>질문 생성</Header>
+      <BottomBar>{page}</BottomBar>
+    </>
+  );
+};
