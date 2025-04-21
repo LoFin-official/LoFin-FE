ff427f1b95e03f443ce7dc782532a26febe0a9e6
wkdtpdlf <sheshe1809@gmail.com>
2025-04-21 14:17:28 +0900

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import QuestionItem from '@/components/questionitem';
+import QuestionItem from '@/components/QuestionItem';
 
 export default function Home() {
   return <div className='text-xl'></div>;
@@ -7,7 +7,7 @@ export default function Home() {
 
 Home.getLayout = (page: ReactNode) => {
   return (
-    <QuestionItem number='01' question='서로의 첫인상은 어땠어?' isComplete={true} detailPageUrl='/main'>
+    <QuestionItem number='01' question='서로의 첫인상은 어땠어?' isComplete={true} detailPageUrl='/QuestionPage'>
       <span>이 안에 자식 컴포넌트도 넣을 수 있어요</span>
     </QuestionItem>
   );
