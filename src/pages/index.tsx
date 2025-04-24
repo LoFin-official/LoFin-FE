eda11fd58c82db8dd153b2eb78c54b2d7d732e79
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-04-24 20:16:25 +0900

    📝 Update : 인풋 컴포넌트 적용 예시

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import BottomBar from '@/components/shared/BottomBar';
 import Button from '@/components/shared/Button';
 import Header from '@/components/shared/Header';
+import Input from '@/components/shared/Input';
 import { ReactNode } from 'react';
 
 export default function Home() {
-  return <div className='text-xl'></div>;
+  return (
+    <div className='text-xl'>
+      <Input label='이메일' placeholder='회원가입에 사용한 이메일을 입력해 주세요.' />
+      <Input label='이메일' placeholder='회원가입에 사용한 이메일을 입력해 주세요.' width='w-[348px]' />
+      <Input
+        label='이메일'
+        placeholder='회원가입에 사용한 이메일을 입력해 주세요.'
+        width='w-[348px]'
+        helperText='· 이메일은 123456@naver.com 형식입니다.'
+      />
+    </div>
+  );
 }
 
 Home.getLayout = (page: ReactNode) => {
@@ -12,7 +24,8 @@ Home.getLayout = (page: ReactNode) => {
     <>
       <Header>안녕</Header>
       <BottomBar>
-        <Button isComplete={true}>확인{page}</Button>
+        {page}
+        <Button isComplete={true}>확인</Button>
       </BottomBar>
     </>
   );
