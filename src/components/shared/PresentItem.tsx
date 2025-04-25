412b4dae4c80b837635c917a2e7593be8ce1c4a6
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-04-25 18:22:42 +0900

    📝 Update : 공통 컴포넌트 - 선물 아이템 마진 패딩 조정

--- src/components/shared/PresentItem.tsx
+++ src/components/shared/PresentItem.tsx
@@ -9,27 +9,26 @@ interface PresentItemProps {
 export default function PresentItem({ sectionTitle, sectionSubtitle, items }: PresentItemProps) {
   return (
     <div>
-      <div className='flex flex-col gap-[2px]'>
+      <div className='flex flex-col gap-[2px] mb-4'>
         {/* 섹션 제목 */}
-        <div className='ml-6 text-[#333333] text-xl font-bold '>{sectionTitle}</div>
+        <div className='ml-2 text-[#333333] text-xl font-bold '>{sectionTitle}</div>
         {/* 섹션 부제목 */}
-        <div className='ml-6 text-[#767676] mb-4'>{sectionSubtitle}</div>
+        {sectionSubtitle && <div className='ml-2 text-[#767676]'>{sectionSubtitle}</div>}
       </div>
 
       {/* 선물 리스트 */}
-      <div className='space-y-4 px-4'>
+      <div className='flex flex-col gap-4'>
         {items.map((item, index) => (
           <div
             key={index}
             className='bg-[#ffffff] rounded-[16px] px-6 py-4'
             style={{
               width: '348px',
-              paddingBottom: '4px',
             }}
           >
             <div className='text-[#999999]'>{item.label}</div>
             <div className='text-[#333333] text-lg ml-1 break-words'>{item.title}</div>
-            <div className='text-[#767676] mb-4 ml-1.5 break-words'>{item.description}</div>
+            <div className='text-[#767676] ml-1.5 break-words'>{item.description}</div>
           </div>
         ))}
       </div>
