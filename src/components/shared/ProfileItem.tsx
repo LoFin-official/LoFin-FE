fd2345b14de7a10519f6ce4a5c7828985479a5da
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-04-25 13:59:27 +0900

    📝 Update : 타입별 출력 텍스트 변경 및 스타일 - rounded 추가

--- src/components/shared/ProfileItem.tsx
+++ src/components/shared/ProfileItem.tsx
@@ -1,11 +1,14 @@
 import { HeartIcon, VitalIcon } from '@/assets/icons/SvgIcon';
 import React from 'react';
+import { useRouter } from 'next/router';
 
 interface ProfileProps {
   type?: 'profile' | 'date' | 'none';
 }
 
 export default function ProfileItem({ type = 'none' }: ProfileProps) {
+  const router = useRouter();
+
   const today = (() => {
     const now = new Date();
     const weekday = now.toLocaleDateString('ko-KR', { weekday: 'short' });
@@ -18,13 +21,22 @@ export default function ProfileItem({ type = 'none' }: ProfileProps) {
     return `${date} (${weekday})`;
   })();
 
+  const handleClick = () => {
+    if (type === 'profile') {
+      router.push('/my-page/profile');
+    }
+  };
+
   return (
     <>
-      <div className='w-[380px] h-[auto] px-6 py-6 flex flex-col mx-auto'>
+      <div
+        className={`w-[380px] h-auto px-6 py-6 bg-[#ffffff] rounded-[18px] flex flex-col mx-auto ${type === 'profile' ? 'cursor-pointer' : ''}`}
+        onClick={handleClick}
+      >
         <div className='w-[332px] h-[120px] flex flex-row gap-[30px] items-center justify-center'>
-          <div className='w-[120px] h-[120px]'></div>
+          <div className='w-[120px] h-[120px] rounded-full'></div>
           <VitalIcon />
-          <div className='w-[120px] h-[120px]'></div>
+          <div className='w-[120px] h-[120px] rounded-full'></div>
         </div>
         <div className='w-[332px] h-6 mt-2 flex items-center justify-center'>
           <span className='flex-1 text-right pr-2 text-xl font-bold text-[#333333]'>제리</span>
