327400e507dfb5f64a78064ff5a2ccd3d3107799
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-04-25 17:13:18 +0900

    🌟 Feat : 마이페이지 각 페이지 추가 및 컴포넌트 적용

--- src/pages/my-page/index.tsx
+++ src/pages/my-page/index.tsx
@@ -1,9 +1,20 @@
 import BottomBar from '@/components/shared/BottomBar';
+import ProfileItem from '@/components/shared/ProfileItem';
 import React from 'react';
 import { ReactNode } from 'react';
 
 export default function MyPage() {
-  return <div>마이페이지입니당.</div>;
+  return (
+    <div className='bg-[#ffd9e1]/35 min-h-[calc(100vh-56px)]'>
+      <div className='h-8 px-6 pt-2 mb-8 flex flex-row font-bold items-center'>
+        <span className='flex-1 text-left text-[#333333] text-2xl'>마이페이지</span>
+        <span className='flex-1 text-right text-[#FF9BB3] text-lg'>DDay</span>
+      </div>
+      <div className='flex flex-col gap-4'>
+        <ProfileItem type='profile'></ProfileItem>
+      </div>
+    </div>
+  );
 }
 
 MyPage.getLayout = (page: ReactNode) => {
