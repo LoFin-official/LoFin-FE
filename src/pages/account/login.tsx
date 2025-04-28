1fb76c15e0b577accb465d903260de72a9541a56
dlgkdud4759 <dlgkdud4759@naver.com>
2025-04-29 00:31:55 +0900

    📝 Update : 로그인 페이지 임시 마크업 완료

--- src/pages/account/login.tsx
+++ src/pages/account/login.tsx
@@ -1,7 +1,8 @@
 import Button from '@/components/shared/Button';
 import Header from '@/components/shared/Header';
 import Input from '@/components/shared/Input';
-import React, { useState } from 'react';
+import React, { ReactNode, useState } from 'react';
+import Image from 'next/image';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
@@ -11,11 +12,10 @@ export default function LoginPage() {
 
   return (
     <>
-      <Header>로그인</Header>
-
       {/* Header 아래 영역 전체를 채우도록 설정 */}
       <div className='flex flex-col items-center justify-center gap-8 px-4 py-10 min-h-[calc(100vh-56px)]'>
         {/* 헤더 높이가 64px이라고 가정 (필요 시 조정) */}
+        <Image src='/images/LoFin.png' alt='LoFin' width={250} height={250} />
         <div className='flex flex-col gap-8 w-full max-w-sm'>
           {/* 인풋 영역 */}
           <div className='flex flex-col gap-8'>
@@ -33,3 +33,12 @@ export default function LoginPage() {
     </>
   );
 }
+
+LoginPage.getLayout = (page: ReactNode) => {
+  return (
+    <>
+      <Header>로그인</Header>
+      {page}
+    </>
+  );
+};
