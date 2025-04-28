6281ce50e6eef261f1592da1c65d395836435695
dlgkdud4759 <dlgkdud4759@naver.com>
2025-04-29 00:29:51 +0900

    📝 Update : 스플레시 페이지 마크업 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,27 @@
-import BottomBar from '@/components/shared/BottomBar';
 import Button from '@/components/shared/Button';
-import Header from '@/components/shared/Header';
-import Input from '@/components/shared/Input';
-import ProfileItem from '@/components/shared/ProfileItem';
+import Image from 'next/image';
 import { ReactNode } from 'react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='text-xl'>
-      <Input label='이메일' placeholder='회원가입에 사용한 이메일을 입력해 주세요.' />
-      <Input label='이메일' placeholder='회원가입에 사용한 이메일을 입력해 주세요.' width='w-[348px]' />
-      <Input
-        label='이메일'
-        placeholder='회원가입에 사용한 이메일을 입력해 주세요.'
-        width='w-[348px]'
-        helperText='· 이메일은 123456@naver.com 형식입니다.'
-      />
-      <ProfileItem></ProfileItem>
-      <ProfileItem type='profile'></ProfileItem>
-      <ProfileItem type='date'></ProfileItem>
-      <ProfileItem type='question'></ProfileItem>
-    </div>
+    <>
+      <div className='flex flex-col items-center justify-center gap-16 min-h-[calc(100vh-148px)]'>
+        <Image src='/images/LoFin.png' alt='LoFin' width={300} height={300} />
+      </div>
+      <div className='flex flex-col gap-4 items-center justify-center'>
+        <div className='flex-grow'></div>
+        <Link href='/account/login'>
+          <Button isComplete={true}>로그인</Button>
+        </Link>
+        <Link href='/account/register'>
+          <div className='h-5 text-base text-[#333333] cursor-pointer'>회원가입</div>
+        </Link>
+      </div>
+    </>
   );
 }
 
 Home.getLayout = (page: ReactNode) => {
-  return (
-    <>
-      <Header>안녕</Header>
-      <BottomBar>
-        {page}
-        <Button isComplete={true}>확인</Button>
-      </BottomBar>
-    </>
-  );
+  return <>{page}</>;
 };
