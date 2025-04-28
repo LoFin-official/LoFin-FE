52458cbfbeca23eafc423b7e2fc3d7885fcc9eac
dlgkdud4759 <dlgkdud4759@naver.com>
2025-04-29 01:26:35 +0900

    🎨 Style : w-[380px]및 w-full 코드 겹침 수정

--- src/components/shared/NoItemText.tsx
+++ src/components/shared/NoItemText.tsx
@@ -7,7 +7,7 @@ interface TextProps {
 
 export default function NoItemText({ title, subtitle }: TextProps) {
   return (
-    <div className='w-[380px] flex flex-col gap-1 min-h-[calc(100vh-112px)] text-center justify-center w-full max-w-sm mx-auto'>
+    <div className='w-[380px] flex flex-col gap-1 min-h-[calc(100vh-112px)] text-center justify-center max-w-sm mx-auto'>
       <div className='h-6 text-[#767676] text-xl font-bold'>{title}</div>
       <div className='h-5 text-[#999999] text-sm'>{subtitle}</div>
     </div>
