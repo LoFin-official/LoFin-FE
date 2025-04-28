c6a10083ff69bc6c03990c7a9b447103a56a1613
dlgkdud4759 <dlgkdud4759@naver.com>
2025-04-28 14:06:50 +0900

    📝 Update : 인풋 컴포넌트 - 클리어 아이콘이 나타나지 않는 오류 수정

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -6,19 +6,34 @@ interface CustomInputProps {
   placeholder?: string;
   width?: string;
   helperText?: string;
+  value?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ label, placeholder, width = 'w-[380px]', helperText }: CustomInputProps) {
+export default function Input({ label, placeholder, width = 'w-[380px]', helperText, value, onChange, }: CustomInputProps) {
   const [isFocused, setIsFocused] = useState(false);
-  const [value, setValue] = useState('');
+  const [internalValue, setInternalValue] = useState(value);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+    setInternalValue(e.target.value);
+    if (onChange) {
+      onChange(e);
+    }
   };
 
   const handleClear = () => {
-    setValue('');
+    setInternalValue('');
+    if (onChange) {
+      const event = {
+        target: { value: '' }
+      } as React.ChangeEvent<HTMLInputElement>;
+      onChange(event);
+    }
   };
+
+  React.useEffect(() => {
+    setInternalValue(value);
+  }, [value]);
 
   return (
     <div className={`${width} h-auto flex flex-col mx-auto`}>
@@ -26,14 +41,14 @@ export default function Input({ label, placeholder, width = 'w-[380px]', helperT
       <div className={`flex flex-row ${width} h-9 border-b ${isFocused ? 'border-[#FF9BB3]' : 'border-[#ccc]'}`}>
         <input
           type='text'
-          value={value}
+          value={internalValue}
           className='w-full h-5 my-2 mx-[2px] bg-transparent outline-none text-base text-left text-[#333333] placeholder-[#cccccc]'
           onChange={handleChange}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
           placeholder={placeholder}
         />
-        {value && (
+        {internalValue && (
           <div className='cursor-pointer mx-[2px] mt-[6px] mb-[6px]' onClick={handleClear}>
             <ClearIcon />
           </div>
