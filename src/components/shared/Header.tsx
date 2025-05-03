94a312402d1cf2c68031375f35f7e00cab3a66b7
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-03 23:15:30 +0900

    📝 Update : 공통 컴포넌트 - 헤더 아이콘 추가 및 건너뛰기 버튼 추가

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import React, { ReactNode } from 'react';
-import { BackIcon, CloseIcon } from '@/assets/icons/SvgIcon';
+import { BackIcon, CloseIcon, MemoryIcon, MemoryImageIcon, QuestionEditIcon } from '@/assets/icons/SvgIcon';
 
 interface HeaderProps {
   children: ReactNode;
@@ -13,6 +13,10 @@ export default function Header({ children }: HeaderProps) {
   // 추억, 질문 작성 = X / 경로 수정 예정
   const noIconPages = ['/memory', '/question', '/present', '/account/profile', '/account/wish', '/account/couple-connect'];
   const closeIconPages = ['/question/edit'];
+  const memoryEditPages = ['/memory'];
+  const questionEditPages = ['/question'];
+  const imagePages = ['/memory/edit'];
+  const nextPages = ['/account/WishForm'];
 
   let IconComponent: React.ElementType | null = BackIcon;
 
@@ -22,10 +26,27 @@ export default function Header({ children }: HeaderProps) {
     IconComponent = CloseIcon;
   }
 
+  let RightComponent: React.ReactNode = null;
+
+  if (memoryEditPages.includes(pathname)) {
+    RightComponent = <MemoryIcon className='cursor-pointer' />;
+  } else if (questionEditPages.includes(pathname)) {
+    RightComponent = <QuestionEditIcon className='cursor-pointer' />;
+  } else if (imagePages.includes(pathname)) {
+    RightComponent = <MemoryImageIcon className='cursor-pointer' />;
+  } else if (nextPages.includes(pathname)) {
+    RightComponent = (
+      <button className='h-5 text-sm text-[#767676] cursor-pointer' onClick={() => router.push('/account/couple-connect')}>
+        건너뛰기
+      </button>
+    );
+  }
+
   return (
-    <div className='w-[412px] h-[56px] relative bg-white border-b border-[#eee] flex items-center'>
+    <div className='w-[412px] h-[56px] relative bg-white border-b border-[#eee] flex items-center justify-between px-2'>
       <div className='cursor-pointer pl-2'>{IconComponent && <IconComponent onClick={() => router.back()} />}</div>
       <div className='absolute left-1/2 transform -translate-x-1/2 text-2xl font-bold text-[#333333]'>{children}</div>
+      <div className='text-right pr-2'>{RightComponent}</div>
     </div>
   );
 }
