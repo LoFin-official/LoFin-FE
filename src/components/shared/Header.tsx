8ebecbf411bf66f594857e2f2b6f73885e8c31c1
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-06 13:50:14 +0900

    📝 Update : 공통 컴포넌트 - 헤더 : 프로필 설정에서 사용될 Props 추가

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -4,9 +4,12 @@ import { BackIcon, CloseIcon, MemoryIcon, MemoryImageIcon, QuestionEditIcon } fr
 
 interface HeaderProps {
   children: ReactNode;
+  onBack?: () => void; // 프로필 설정 페이지
+  rightElement?: ReactNode; // 건너뛰기 버튼 등등
+  showBackButton?: boolean; // 뒤로가기 보일지 여부
 }
 
-export default function Header({ children }: HeaderProps) {
+export default function Header({ children, onBack, showBackButton, rightElement }: HeaderProps) {
   const router = useRouter();
   const { pathname } = router;
 
@@ -18,33 +21,39 @@ export default function Header({ children }: HeaderProps) {
   const imagePages = ['/memory/edit'];
   const nextPages = ['/account/WishForm'];
 
+  // 뒤로가기 아이콘 설정
   let IconComponent: React.ElementType | null = BackIcon;
 
   if (noIconPages.includes(pathname)) {
+    IconComponent = null;
+  } else if (showBackButton === false) {
     IconComponent = null;
   } else if (closeIconPages.includes(pathname)) {
     IconComponent = CloseIcon;
   }
 
-  let RightComponent: React.ReactNode = null;
+  // 오른쪽 요소 설정
+  let RightComponent: React.ReactNode = rightElement;
 
-  if (memoryEditPages.includes(pathname)) {
-    RightComponent = <MemoryIcon className='cursor-pointer' />;
-  } else if (questionEditPages.includes(pathname)) {
-    RightComponent = <QuestionEditIcon className='cursor-pointer' />;
-  } else if (imagePages.includes(pathname)) {
-    RightComponent = <MemoryImageIcon className='cursor-pointer' />;
-  } else if (nextPages.includes(pathname)) {
-    RightComponent = (
-      <button className='h-5 text-sm text-[#767676] cursor-pointer' onClick={() => router.push('/account/couple-connect')}>
-        건너뛰기
-      </button>
-    );
+  if (RightComponent === undefined) {
+    if (memoryEditPages.includes(pathname)) {
+      RightComponent = <MemoryIcon className='cursor-pointer' />;
+    } else if (questionEditPages.includes(pathname)) {
+      RightComponent = <QuestionEditIcon className='cursor-pointer' />;
+    } else if (imagePages.includes(pathname)) {
+      RightComponent = <MemoryImageIcon className='cursor-pointer' />;
+    } else if (nextPages.includes(pathname)) {
+      RightComponent = (
+        <button className='h-5 text-sm text-[#767676] cursor-pointer' onClick={() => router.push('/account/couple-connect')}>
+          건너뛰기
+        </button>
+      );
+    }
   }
 
   return (
     <div className='w-[412px] h-[56px] relative bg-white border-b border-[#eee] flex items-center justify-between px-2'>
-      <div className='cursor-pointer pl-2'>{IconComponent && <IconComponent onClick={() => router.back()} />}</div>
+      <div className='cursor-pointer pl-2'>{IconComponent && <IconComponent onClick={onBack ?? (() => router.back())} />}</div>
       <div className='absolute left-1/2 transform -translate-x-1/2 text-2xl font-bold text-[#333333]'>{children}</div>
       <div className='text-right pr-2'>{RightComponent}</div>
     </div>
