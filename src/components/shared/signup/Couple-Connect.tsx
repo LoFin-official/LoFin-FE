0e4bc8bbd37dc6bd901df32ea73276ec6e52db16
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-08 17:27:41 +0900

    📝 Update : 커플 연결 - 초대 코드가 올바른 경우 커플 매칭 후 페이지 이동

--- src/components/shared/signup/Couple-Connect.tsx
+++ src/components/shared/signup/Couple-Connect.tsx
@@ -1,11 +1,34 @@
 import { ProgressIcon, ProgressingIcon } from '@/assets/icons/SvgIcon';
 import Button from '@/components/shared/Button';
-import React, { ReactNode, useState } from 'react';
+import React, { useState } from 'react';
+import Input from '../Input';
 
-export default function CoupleConnectPage({ onNext }: { onNext: () => void }) {
-  const [currentStep, setCurrentStep] = useState(2);
+export default function CoupleConnectPage({ onNext, currentStep }: { onNext: () => void; currentStep: number }) {
+  const [isConnected, setIsConnected] = useState(false);
+  const [code, setCode] = useState('');
+  const [error, setError] = useState(false);
 
   const steps = ['1', '2', '3', '4'];
+
+  const handleConnect = () => {
+    if (code === 'QWE') {
+      setIsConnected(true);
+      setError(false);
+      onNext();
+    } else {
+      setError(true);
+    }
+  };
+
+  const handleCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value.toUpperCase(); // 대문자로 변환
+    const alphanumericOnly = input.replace(/[^A-Z0-9]/g, ''); // 영문 대문자 + 숫자만 허용
+    setCode(alphanumericOnly);
+    setError(false);
+  };
+
+  const isComplete = code.length === 10;
+
   return (
     <>
       <div className='flex flex-col min-h-[calc(100vh-56px)] pt-8 justify-between'>
@@ -19,9 +42,32 @@ export default function CoupleConnectPage({ onNext }: { onNext: () => void }) {
                 </div>
               ))}
             </div>
+            <div className='flex flex-col gap-0.5 w-[380px] text-center'>
+              <span className='text-[#333333] text-xl font-bold'>
+                당신의 초대 코드를 공유하거나,
+                <br />
+                상대방의 코드를 입력해 연결하세요!
+              </span>
+            </div>
+            <div className='flex flex-col gap-2'>
+              <span className='h-6 text-center text-[#ff9bb3] text-xl'>당신의 코드</span>
+              <div className='w-[134px] h-10 items-center text-center px-0.5 py-2 border-b border-[#ff9bb3]'>
+                <span className='text-[#333333] font-bold text-xl'>E2R1TWSF9V</span>
+              </div>
+            </div>
+            <div className='flex flex-col gap-1'>
+              <Input
+                label='상대방의 초대 코드를 받으셨나요?'
+                placeholder='상대방의 초대 코드를 입력해 주세요.'
+                value={code}
+                maxLength={10}
+                onChange={handleCodeChange}
+              />
+              {error && <div className='text-[#FF2A2A] text-sm ml-0.5'>초대 코드가 올바르지 않습니다. 다시 입력해 주세요.</div>}
+            </div>
           </div>
         </div>
-        <Button isComplete={false} onClick={onNext} className='mb-4'>
+        <Button isComplete={isComplete} onClick={handleConnect} className='mb-4'>
           연결하기
         </Button>
       </div>
