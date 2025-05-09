be1c18e5448b2933ac01c826b9a14e899b66dfa2
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-09 22:41:56 +0900

    📝 Update : 질문 생성 페이지 임시 마크업 완료

--- src/pages/question/edit/index.tsx
+++ src/pages/question/edit/index.tsx
@@ -1,7 +1,8 @@
 import QuestionTabs from '@/components/shared/question/QuestionTabs';
 import BottomBar from '@/components/shared/BottomBar';
 import Header from '@/components/shared/Header';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import Input from '@/components/shared/Input';
 import Button from '@/components/shared/Button';
@@ -9,8 +10,34 @@ import Button from '@/components/shared/Button';
 export default function QuestionEditPage() {
   const [selectedTab, setSelectedTab] = useState<'direct' | 'random'>('direct');
   const [question, setQuestion] = useState('');
+  const router = useRouter();
 
   const isComplete = question.trim() !== '';
+
+  const [randomQuestion, setRandomQuestion] = useState('');
+
+  const getRandomQuestion = () => {
+    const index = Math.floor(Math.random() * randomQuestions.length);
+    setRandomQuestion(randomQuestions[index]);
+  };
+
+  useEffect(() => {
+    if (selectedTab === 'random') getRandomQuestion();
+  }, [selectedTab]);
+
+  const randomQuestions = [
+    '서로를 처음 봤을 때 어떤 인상이었나요?',
+    '요즘 가장 즐겨 듣는 노래는?',
+    '하루 중 가장 좋아하는 시간대는 언제인가요?',
+    '같이 가보고 싶은 여행지는?',
+  ];
+
+  const handleQuestion = () => {
+    //API 요청 예정
+    if (isComplete) {
+      router.push('/question');
+    }
+  };
 
   return (
     <div>
@@ -34,7 +61,7 @@ export default function QuestionEditPage() {
           </div>
 
           <div className='flex-grow' />
-          <Button isComplete={isComplete} className='mb-4'>
+          <Button isComplete={isComplete} onClick={handleQuestion} className='mb-4'>
             질문 생성
           </Button>
         </div>
@@ -47,12 +74,14 @@ export default function QuestionEditPage() {
               <div className='h-6 text-[#333333] text-xl font-bold'>지금, 우리를 위한 질문을 골라봤어요.</div>
               <div className='h-5 text-[#767676]'>지금의 우리에게 딱 맞는 질문일지도 몰라요.</div>
             </div>
-            <Input label='운명처럼 등장한 질문!' placeholder='사소해도 좋아요, 궁금했던 걸 남겨보세요.' width='w-[348px]' />
+            <Input label='운명처럼 등장한 질문!' width='w-[348px]' value={randomQuestion} readOnly alwaysActiveStyle />
           </div>
-          <div className='text-[#767676] text-sm text-center mt-1'>우리 얘기에 더 어울리는 질문이 궁금해요.</div>
+          <div className='text-[#767676] text-sm text-center mt-1 cursor-pointer' onClick={getRandomQuestion}>
+            우리 얘기에 더 어울리는 질문이 궁금해요.
+          </div>
 
           <div className='flex-grow' />
-          <Button isComplete={true} className='mb-4'>
+          <Button isComplete={true} onClick={handleQuestion} className='mb-4'>
             질문 생성
           </Button>
         </div>
