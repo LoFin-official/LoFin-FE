aeb7253c41bdcaa8608e4ace2c317ea8d76042a4
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-09 22:38:30 +0900

    📝 Update : 공통 컴포넌트 - 인풋 : 주석 추가 및 포커스 되지 않은 상태에서 강조 색상인 경우 추가

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -2,27 +2,31 @@ import React, { useState } from 'react';
 import { ClearIcon, EyeCloseIcon, EyeIcon } from '@/assets/icons/SvgIcon';
 
 interface CustomInputProps {
-  label: string;
-  placeholder?: string;
-  width?: string;
-  helperText?: string;
+  label: string; // 인풋 컴포넌트 타이틀
+  placeholder?: string; // 인풋 컴포넌트 힌트 메시지
+  width?: string; // 인풋 컴포넌트 크기
+  helperText?: string; // 인풋 컴포넌트 도우미
   name?: string;
   value?: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  type?: string;
-  maxLength?: number;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string; // 패스워드용 인풋 타입입
+  maxLength?: number; // 최대 글자 수 제한
+  readOnly?: boolean; // 읽기 전용 텍스트
+  alwaysActiveStyle?: boolean; // 포커스 되지 않아도 강조 색상 유지
 }
 
 export default function Input({
-  label,
-  placeholder,
-  width = 'w-[380px]',
-  helperText,
+  label, // 인풋 컴포넌트 타이틀
+  placeholder, // 인풋 컴포넌트 힌트 메시지
+  width = 'w-[380px]', // 인풋 컴포넌트 크기
+  helperText, // 인풋 컴포넌트 도우미
   name,
   value,
   onChange,
-  type = 'text',
-  maxLength,
+  type = 'text', // 패스워드용 인풋 타입
+  maxLength, // 최대 글자 수 제한
+  readOnly = false, // 읽기 전용 텍스트
+  alwaysActiveStyle = false, // 포커스 되지 않아도 강조 색상 유지
 }: CustomInputProps) {
   const [isFocused, setIsFocused] = useState(false);
   const [internalValue, setInternalValue] = useState(value);
@@ -52,9 +56,10 @@ export default function Input({
 
   return (
     <div className={`${width} h-auto flex flex-col mx-auto`}>
-      <p className={`text-sm h-4 text-left ${isFocused ? 'text-[#FF9BB3]' : 'text-[#999]'}`}>{label}</p>
-      <div className={`flex flex-row ${width} h-9 border-b ${isFocused ? 'border-[#FF9BB3]' : 'border-[#ccc]'}`}>
+      <p className={`text-sm h-4 text-left ${alwaysActiveStyle || isFocused ? 'text-[#FF9BB3]' : 'text-[#999]'}`}>{label}</p>
+      <div className={`flex flex-row ${width} h-9 border-b ${alwaysActiveStyle || isFocused ? 'border-[#FF9BB3]' : 'border-[#ccc]'}`}>
         <input
+          readOnly={readOnly}
           type={isPassword && !showPassword ? 'password' : 'text'}
           name={name}
           value={internalValue}
