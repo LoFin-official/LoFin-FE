3138e047f25bed27e4442573b232bc40427215de
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-09 22:40:22 +0900

    🌟 Feat : 질문 아이템 임시 데이터 추가

---
+++ src/data/questionsData.ts
@@ -0,0 +1,4 @@
+export const questions = [
+  { id: 1, number: 'Q1.', question: '서로를 처음 봤을 때 어떤 인상이었나요?', isComplete: true, detailPageUrl: '/question/1' },
+  { id: 2, number: 'Q2.', question: '요즘 가장 즐겨 듣는 노래는?', isComplete: false, detailPageUrl: '/question/2' },
+];
