d0af306b37584aadae80d6fac0d023619d62c694
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-15 20:37:06 +0900

    🌟 Feat : 공통 컴포넌트 - 추억 아이템 : 4:5 폴라로이드

---
+++ src/components/shared/memory/MemoryPolaroidItem4.tsx
@@ -0,0 +1,37 @@
+import { HeartIcon } from '@/assets/icons/SvgIcon';
+import React from 'react';
+
+interface MemoryData {
+  title: string;
+  text: string;
+  date: string;
+  dday: number;
+}
+
+interface MemoryPolaroidItemProps {
+  data: MemoryData;
+}
+
+export default function MemoryPolaroidItem4({ data }: MemoryPolaroidItemProps) {
+  return (
+    <div className='w-[380px] h-[468px] flex flex-col gap-1 items-center py-4 px-4'>
+      {/* 사진 영역 */}
+      <div className='w-[348px] h-[384px] flex flex-col gap-4'>
+        <div className='w-[348px] h-[300px] bg-[#eeeeee]'></div>
+        <div className='w-[344px] h-[76px] flex flex-col gap-2 px-0.5 mx-0.5'>
+          <span className='h-7 text-xl text-[#333333] font-bold'>{data.title}</span>
+          <span className='h-10 text-[#767676]'>{data.text}</span>
+        </div>
+      </div>
+
+      {/* 좋아요 & 날짜 영역 */}
+      <div className='w-auto h-[48px] flex flex-col justify-center items-center self-end px-0.5'>
+        <div className='flex flex-row gap-0.5 text-[#FF9BB3] items-center'>
+          <HeartIcon className='h-5 w-5' />
+          <span className='h-7 text-lg font-bold'>+ {data.dday}</span>
+        </div>
+        <span className='h-5 text text-[#333333]'>{data.date}</span>
+      </div>
+    </div>
+  );
+}
