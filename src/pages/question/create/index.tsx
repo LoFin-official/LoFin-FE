2f00d7735c294db19afac062739806fff8e07baa
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-17 17:24:44 +0900

    📝 Update : 질문 생성 페이지 - 스타일 조정

--- src/pages/question/create/index.tsx
+++ src/pages/question/create/index.tsx
@@ -46,16 +46,16 @@ export default function QuestionCreatePage() {
       <QuestionTabs selectedTab={selectedTab} onSelectTab={setSelectedTab} />
 
       {selectedTab === 'direct' && (
-        <div className='flex flex-col pt-16 min-h-[calc(100vh-166px)]'>
+        <div className='flex flex-col pt-16 min-h-[calc(100vh-166px)] px-4'>
           <div className='flex flex-col gap-8'>
-            <div className='flex flex-col w-[380px] h-[46px] mx-auto gap-[2px] items-center'>
-              <div className='h-6 text-[#333333] text-xl font-bold'>소소해도 좋아요, 궁금한 마음을 나눠보세요.</div>
+            <div className='flex flex-col w-full max-w-[380px] h-[46px] mx-auto gap-[2px] items-center'>
+              <div className='h-6 text-[#333333] text-xl font-bold'>소소해도 좋아요, 궁금한 마음 나눠보세요.</div>
               <div className='h-5 text-[#767676]'>사소한 질문이 우리를 더 가깝게 해줄 거예요.</div>
             </div>
             <Input
               label='당신의 질문 한 줄'
               placeholder='사소해도 좋아요, 궁금했던 걸 남겨보세요.'
-              width='w-[348px]'
+              width='w-full max-w-[348px]'
               value={question}
               onChange={(e) => setQuestion(e.target.value)}
             />
@@ -69,13 +69,13 @@ export default function QuestionCreatePage() {
       )}
 
       {selectedTab === 'random' && (
-        <div className='flex flex-col pt-16 min-h-[calc(100vh-166px)]'>
+        <div className='flex flex-col pt-16 min-h-[calc(100vh-166px)] px-4'>
           <div className='flex flex-col gap-8'>
-            <div className='flex flex-col w-[380px] h-[46px] mx-auto gap-[2px] items-center'>
+            <div className='flex flex-col w-full max-w-[380px] h-[46px] mx-auto gap-0.5 items-center'>
               <div className='h-6 text-[#333333] text-xl font-bold'>지금, 우리를 위한 질문을 골라봤어요.</div>
               <div className='h-5 text-[#767676]'>지금의 우리에게 딱 맞는 질문일지도 몰라요.</div>
             </div>
-            <Input label='운명처럼 등장한 질문!' width='w-[348px]' value={randomQuestion} readOnly alwaysActiveStyle />
+            <Input label='운명처럼 등장한 질문!' width='w-full max-w-[348px]' value={randomQuestion} readOnly alwaysActiveStyle />
           </div>
           <div className='text-[#767676] text-sm text-center mt-1 cursor-pointer' onClick={getRandomQuestion}>
             우리 얘기에 더 어울리는 질문이 궁금해요.
