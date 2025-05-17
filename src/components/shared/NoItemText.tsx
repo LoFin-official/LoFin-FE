b8690e936278b9a65199ee456ea67c722c9b3747
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-17 17:11:48 +0900

    📝 Update : 공통 컴포넌트 - 아이템이 없는 경우 텍스트 : 스타일 조정

--- src/components/shared/NoItemText.tsx
+++ src/components/shared/NoItemText.tsx
@@ -7,7 +7,7 @@ interface TextProps {
 
 export default function NoItemText({ title, subtitle }: TextProps) {
   return (
-    <div className='w-[380px] flex flex-col gap-1 min-h-[calc(100vh-112px)] text-center justify-center max-w-sm mx-auto'>
+    <div className='w-full min-w-[348px] max-w-[380px] flex flex-col gap-1 min-h-[calc(100vh-112px)] text-center justify-center mx-auto'>
       <div className='h-6 text-[#767676] text-xl font-bold'>{title}</div>
       <div className='h-5 text-[#999999] text-sm'>{subtitle}</div>
     </div>
