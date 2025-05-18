c233e4af5567d71bc1feb33310b4034d550096ef
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-19 02:01:44 +0900

    📝 Update : 공통 컴포넌트 - 바텀 바 : 페이지 이동 방식 수정

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from 'react';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { GiftIcon, ChatIcon, MemoryIcon, QuestionsIcon, MyIcon } from '@/assets/icons/SvgIcon';
 import { stackRouterPush } from '@/store/stackRouter';
@@ -15,45 +14,34 @@ export default function BottomBar({ children }: BottomBarProps) {
   const getActiveClass = (targetPath: string) =>
     pathname === targetPath || pathname.startsWith(targetPath + '/') ? 'text-[#FF9BB3]' : 'text-[#333333]';
 
-  const handleNavigate = () => {
-    // 다른 페이지로 이동
-    stackRouterPush(router, '/present');
+  const handleNavigate = (path: string) => {
+    stackRouterPush(router, path);
   };
 
   return (
     <div>
       {children}
       <div className='fixed bottom-0 w-full max-w-[412px] h-[56px] bg-white border-t flex justify-around items-center'>
-        <Link href={'/present'}>
-          <button className='flex flex-col items-center' onClick={handleNavigate}>
-            <GiftIcon className='fill-current' />
-            <span className='text-xs font-bold'>선물</span>
-          </button>
-        </Link>
-        <Link href={'/chat'}>
-          <button className={`flex flex-col items-center ${getActiveClass('/chat')}`}>
-            <ChatIcon className='fill-current' />
-            <span className='text-xs font-bold'>채팅</span>
-          </button>
-        </Link>
-        <Link href={'/memory'}>
-          <button className={`flex flex-col items-center ${getActiveClass('/memory')}`}>
-            <MemoryIcon className='fill-current' />
-            <span className='text-xs font-bold'>추억</span>
-          </button>
-        </Link>
-        <Link href={'/question'}>
-          <button className={`flex flex-col items-center ${getActiveClass('/question')}`}>
-            <QuestionsIcon className='fill-current w-6 h-6' />
-            <span className='text-xs font-bold'>질문</span>
-          </button>
-        </Link>
-        <Link href={'/my-page'}>
-          <button className={`flex flex-col items-center ${getActiveClass('/my-page')}`}>
-            <MyIcon className='fill-current' />
-            <span className='text-xs font-bold'>마이</span>
-          </button>
-        </Link>
+        <button className={`flex flex-col items-center ${getActiveClass('/present')}`} onClick={() => handleNavigate('/present')}>
+          <GiftIcon className='fill-current' />
+          <span className='text-xs font-bold'>선물</span>
+        </button>
+        <button className={`flex flex-col items-center ${getActiveClass('/chat')}`} onClick={() => handleNavigate('/chat')}>
+          <ChatIcon className='fill-current' />
+          <span className='text-xs font-bold'>채팅</span>
+        </button>
+        <button className={`flex flex-col items-center ${getActiveClass('/memory')}`} onClick={() => handleNavigate('/memory')}>
+          <MemoryIcon className='fill-current' />
+          <span className='text-xs font-bold'>추억</span>
+        </button>
+        <button className={`flex flex-col items-center ${getActiveClass('/question')}`} onClick={() => handleNavigate('/question')}>
+          <QuestionsIcon className='fill-current w-6 h-6' />
+          <span className='text-xs font-bold'>질문</span>
+        </button>
+        <button className={`flex flex-col items-center ${getActiveClass('/my-page')}`} onClick={() => handleNavigate('/my-page')}>
+          <MyIcon className='fill-current' />
+          <span className='text-xs font-bold'>마이</span>
+        </button>
       </div>
     </div>
   );
