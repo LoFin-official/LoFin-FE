40501ffb30ab95f2c69b9152a714cba8dbdfee64
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-19 02:02:00 +0900

    📝 Update : 채팅 페이지 헤더 추가

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import BottomBar from '@/components/shared/BottomBar';
+import Header from '@/components/shared/Header';
+import React, { ReactNode } from 'react';
 
 export default function ChatPage() {
   return <div>채팅페이지입니당.</div>;
 }
+
+ChatPage.getLayout = (page: ReactNode) => {
+  return (
+    <>
+      <Header>톰</Header>
+      {page}
+    </>
+  );
+};
