f75a3be5efe9d0b73a1106b090d99072fbfe9562
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-25 23:52:15 +0900

    📝 Update : <meta> 태그 오류로 인해 _document -> _app 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/calendar.css';
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { ReactNode } from 'react';
+import Head from 'next/head';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactNode) => ReactNode;
@@ -17,8 +18,14 @@ export default function App({
   const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
   return (
-    <div className='w-full h-screen flex justify-center items-center bg-white'>
-      <div className='w-full max-w-[412px] h-full'>{getLayout(<Component {...pageProps} />)}</div>
-    </div>
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
+
+      <div className='w-full h-screen flex justify-center items-center bg-white'>
+        <div className='w-full max-w-[412px] h-full'>{getLayout(<Component {...pageProps} />)}</div>
+      </div>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='ko'>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      </Head>
+      <Head></Head>
       <body className='antialiased'>
         <Main />
         <NextScript />
