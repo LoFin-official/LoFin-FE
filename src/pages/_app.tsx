34db80b6fd9a17576beb7323635bedbeb882fc27
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-26 14:50:38 +0900

    📝 Update : 앱 환경에서 인풋 포커스 시 Zoom In 가능 off

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App({
   return (
     <>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no' />
       </Head>
 
       <div className='w-full h-screen flex justify-center items-center bg-white'>
