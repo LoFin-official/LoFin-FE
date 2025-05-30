4f65d7eeaa8266efae49fb15431b630b3aeb1e16
dlgkdud4759 <dlgkdud4759@naver.com>
2025-05-30 15:25:39 +0900

    📝 Update : 마이페이지 - 위시 : 페이지 마진 조정

--- src/pages/my-page/wish/index.tsx
+++ src/pages/my-page/wish/index.tsx
@@ -154,12 +154,12 @@ export default function WishPage() {
 
   return (
     <>
-      <div className='flex flex-col min-h-[calc(100vh-112px)] pt-16 pb-14 justify-between'>
+      <div className='flex flex-col min-h-[calc(100vh-112px)] pt-16 pb-14 px-4 justify-between'>
         <div className='flex flex-1 justify-center'>
           <div className='flex flex-col gap-8 items-center'>
-            <div className='flex flex-col gap-0.5 w-[380px] text-center'>
+            <div className='flex flex-col gap-0.5 w-full max-w-[380px] text-center'>
               <span className='h-6 text-[#333333] text-xl font-bold'>기념일에 받고 싶은 선물을 선택하세요!</span>
-              <span className='h-5 text-[#767676]'>카테고리에서 원하는 선물을 고르거나 직접 입력할 수도 있어요.</span>
+              <span className='h-5 text-[#767676]'>카테고리에서 선물을 고르거나 직접 입력할 수도 있어요.</span>
             </div>
             {!isLoading && (
               <WishCategoryItem
